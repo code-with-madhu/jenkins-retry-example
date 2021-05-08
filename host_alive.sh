54c6f21fae17e7d79d695789d151674dc618f0bd
code-with-madhu <73005757+code-with-madhu@users.noreply.github.com>
2021-05-08 22:52:59 +0530

    Update host_alive.sh

--- host_alive.sh
+++ host_alive.sh
@@ -2,7 +2,7 @@
 # The script checks whether a host is online or not every 5 seconds. When the host goes online, the script will notify you and stop.
 while :
 do
-  if ping -c 1 ip_address &> /dev/null
+  if ping -c 1 hostname  &> /dev/null
   then
     echo "Host is online"
     break
